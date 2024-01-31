4215a1878790c2aadebd993f1124e8199c77b281
coolwhipki <kirajanegirl@gmail.com>
2024-01-31 09:58:50 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//Welcome user to game
+Console.WriteLine("Welcome to Tic-Tac-Toe!!");
+Console.WriteLine("Player 1 (X) - Player 2 (O)");
+
+
+
+// Create a game board array to store the players’ choices
+// char[] board = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+
+List<string> words = new List<string>()
+            {
+                " ", " ", " ",
+                " ", " ", " ",
+                " ", " ", " ",
+            };
+
+//Ask each player in turn for their choice and update the game board array
+//if statement: if team 1 then 'X' if team 2 'O'
+//if that spot already has something in it print something saying "no"
+//update the game board array
+
+
+//Print the board by calling the method in the supporting class
+
+//Check for a winner by calling the method in the supporting class, and notify the players when a win has occurred and which player won the game
