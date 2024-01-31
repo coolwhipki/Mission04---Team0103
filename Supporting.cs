f812a92e1acbbf0e56e22552ffa49e6ba5ccacd1
RumitOsu <timurkherlenbayar@gmail.com>
2024-01-31 15:47:46 -0700

    haha i fixed the allignment

--- Supporting.cs
+++ Supporting.cs
@@ -42,15 +42,15 @@ namespace Mission04___Team0103
             // Winning conditions
             int[,] winConditions = new int[,]
             {
-        {0, 1, 2}, // Row 1
-        {3, 4, 5}, // Row 2
-        {6, 7, 8}, // Row 3
-        {0, 3, 6}, // Column 1
-        {1, 4, 7}, // Column 2
-        {2, 5, 8}, // Column 3
-        {0, 4, 8}, // Diagonal 1
-        {2, 4, 6}  // Diagonal 2
-            };
+                {0, 1, 2}, // Row 1
+                {3, 4, 5}, // Row 2
+                {6, 7, 8}, // Row 3
+                {0, 3, 6}, // Column 1
+                {1, 4, 7}, // Column 2
+                {2, 5, 8}, // Column 3
+                {0, 4, 8}, // Diagonal 1
+                {2, 4, 6}  // Diagonal 2
+             };
 
             for (int i = 0; i < 8; i++)
             {
