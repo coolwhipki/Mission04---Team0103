ee3cf1b68c530c0c8281cffc797b03d4c9bbe325
RumitOsu <103869313+RumitOsu@users.noreply.github.com>
2024-01-31 12:07:46 -0700

    finished printboard method

--- Supporting.cs
+++ Supporting.cs
@@ -8,9 +8,31 @@ namespace Mission04___Team0103
 {
     internal class Supporting
     {
-        PrintBoard():
+        public void PrintBoard(char[] board)
         {
-
+            // iterate through every cell of the board / every value of the array
+            for (int i = 0; i < board.Length; i++)
+            {
+                // print out the value of the current cell
+                Console.Write($" {board[i]} ");
+                // if current cell is the last cell in the row (cell 3, 6, or 9), proceed
+                if ((i + 1) % 3 == 0)
+                {
+                    // create a new line
+                    Console.WriteLine();
+                    // if current cell is not the last cell of the board (cell 9), proceed
+                    if (i < board.Length - 1)
+                    {
+                        // print a row seperator
+                        Console.WriteLine("---|---|---");
+                    }
+                }
+                else
+                {
+                    // else, print a column seperator
+                    Console.Write("|");
+                }
+            }
         }
 
         IfWinner():
