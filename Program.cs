945d871a6c244431af71774e473a87eb688ac64c
coolwhipki <kirajanegirl@gmail.com>
2024-02-01 17:36:33 -0700

    validated inputs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿// Create a game board array to store the players’ choices
+﻿using Mission04___Team0103;
+
+
+Supporting s = new Supporting();
+
+// Create a game board array to store the players’ choices
 char[] boardArray = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
 // initialize player 1's turn
 int playerTurn = 0;
@@ -18,7 +23,7 @@ do
         Console.WriteLine($"Player {playerTurn + 1}, what spot do you want");
 
         // Print the board by calling the method in the supporting class
-        TimursPrintMethod(boardArray);
+        s.PrintBoard(boardArray);
 
     } while (playGame(playerTurn));
 
@@ -34,11 +39,11 @@ do
         } */
 
     // Check for a winner by calling the method in the supporting class,
-    gameResult = MosCheckWinnerMethod(boardArray);
+    gameResult = s.CheckWinner(boardArray);
 } while (gameResult == null);
 
 // Print the board by calling the method in the supporting class
-TimursPrintMethod(boardArray);
+s.PrintBoard(boardArray);
 // Display the Winner! notify the players when a win has occurred and which player won the game
 Console.WriteLine(gameResult);
 
@@ -46,6 +51,9 @@ Console.WriteLine(gameResult);
 // Player Turn Function
 bool playGame(int pT)
 {
+    // set a player choice variable integer
+    int playerChoice = 0;
+
     // set playerLetter (shortened version)
     char playerLetter = pT == 0 ? 'X' : 'O';
 
@@ -58,21 +66,33 @@ bool playGame(int pT)
            playerLetter = 'O';
        } */
 
-    Console.Write("Enter which square to play on! (enter a number 1-9): ");
-    int playerChoice = (int.Parse(Console.ReadLine()) - 1);
-
-    // update the game board array with player choice, if valid
-    if (playerChoice >= 0 && playerChoice <= 8 && boardArray[playerChoice] == ' ')
+    //Ensure input is valid
+    while (true)
     {
-        boardArray[playerChoice] = playerLetter;
-        return false;
-    }
-    else
-    {
-        Console.WriteLine("Oops! That wasn't a valid spot, try again");
-        return true;
-    }
 
+        Console.Write("Enter which square to play on! (enter a number 1-9): ");
+        string input = Console.ReadLine();
+
+
+        if (int.TryParse(input, out playerChoice) && playerChoice >= 1 && playerChoice <= 9)
+        {
+            playerChoice--; // Minus 1 from input to make sure it is corresponding to correct position in the array
+
+            // Check if the selected square is available
+            if (boardArray[playerChoice] == ' ')
+            {
+                boardArray[playerChoice] = playerLetter;
+                return false; // Valid input, exit the loop
+            }
+            else
+            {
+                Console.WriteLine("Oops! That spot is already taken, try again");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a number between 1 and 9.");
+        }
+    }
 }
-
 
